b6edc909b73599304e4bd158c3e9147af0b1213b
gautamkmahato <gautamkmahato@gmail.com>
2025-06-19 13:21:16 +0530

    updated the contact and privacy page

--- app/(legal)/contact/page.jsx
+++ app/(legal)/contact/page.jsx
@@ -8,9 +8,10 @@ export default function ContactPage() {
       <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
       <p className="mb-4">For any queries or support, reach out to us:</p>
       <ul className="list-disc pl-6">
+        <li>Name: Gautam Mahato</li>
         <li>Email: gkmbusiness74@gmail.com</li>
         <li>Phone: +91-834-0605124</li>
-        <li>Address: Vill Krishnapur, Seraikella, India</li>
+        <li>Address: Vill Krishnapur, Seraikella Kharsawan, 833101, Jharkhand, India</li>
       </ul>
     </main>
     </>
