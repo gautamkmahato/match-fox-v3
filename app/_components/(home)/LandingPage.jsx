77246a3f4d3729b29354362906a3adf20c7c387c
gautamkmahato <gautamkmahato@gmail.com>
2025-06-21 18:25:13 +0530

    updated homepage

--- app/_components/(home)/LandingPage.jsx
+++ app/_components/(home)/LandingPage.jsx
@@ -241,7 +241,7 @@ export default function LandingPage() {
               transition={{ duration: 0.8 }}
             >
               <a className="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 ps-3 rounded-full transition hover:border-gray-300 dark:bg-neutral-800 dark:border-neutral-700 dark:hover:border-neutral-600 dark:text-neutral-200"
-                href="tubeguruji.com" target="_blank">
+                href="hirenom.com" target="_blank">
                 HIRENOM: Best Interview Copilot
                 <span className="py-1.5 px-2.5 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-200 font-semibold text-sm text-gray-600 dark:bg-neutral-700 dark:text-neutral-400">
                   <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6" /></svg>
@@ -295,13 +295,13 @@ export default function LandingPage() {
               Start Free Interview
               <ArrowRight className="w-4 h-4" />
             </Link>
-            <Link
+            {/* <Link
               href="/sign-up"
               className="px-6 py-2.5 sm:px-8 sm:py-3 text-sm font-semibold text-gray-800 dark:text-gray-200 bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 rounded-lg shadow-md hover:shadow w-full sm:w-auto transition-all"
               aria-label="Watch a demo of how the platform works"
             >
               Watch Demo
-            </Link>
+            </Link> */}
           </motion.div>
 
 
