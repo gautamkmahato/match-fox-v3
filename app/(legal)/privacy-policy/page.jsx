74d97130042fb54ed2957541dd591ce64aeb9abd
gautamkmahato <gautamkmahato@gmail.com>
2025-06-21 18:33:13 +0530

    updated homepage

--- app/(legal)/privacy-policy/page.jsx
+++ app/(legal)/privacy-policy/page.jsx
@@ -7,7 +7,7 @@ export default function PrivacyPolicyPage() {
     <Header />
     <main className="max-w-4xl mx-auto p-6 sm:p-12 bg-white shadow-md rounded-md">
       <h1 className="text-3xl font-bold mb-6">Privacy Policy</h1>
-      <p className="mb-4 font-semibold">Effective Date: [Insert Date]</p>
+
 
       <section className="mb-6">
         <h2 className="text-2xl font-semibold mb-2">What Data We Collect</h2>
