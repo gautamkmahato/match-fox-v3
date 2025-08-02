0f97cbda516d64e5c4f3b987061099c100ec6fa6
gautamkmahato <gautamkmahato@gmail.com>
2025-08-02 17:50:38 +0530

    updated the payment page

--- test.js
+++ test.js
@@ -5,12 +5,12 @@ import { Polar } from "@polar-sh/sdk";
 export default async function polarTest() {
 
     const polar = new Polar({
-        accessToken: "polar_oat_V1ElbSZXCYCDgwJsoW3KcTrlTOjEsQZbzGMm73cuGHS",
+        accessToken: POLAR_TOKEN,
         
     });
 
     const checkout = await polar.checkouts.create({
-        products: ["8a1b3b5b-b82c-447a-b437-d1155645801d"],
+        products: [process.env.POLAR_PRODUCT_ID],
         successUrl: process.env.POLAR_SUCCESS_URL
     });
     console.log(checkout.url);
